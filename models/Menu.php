4a4c8a9237ce509e5a3b0cc1fc4b9598d8f9e06b
Fitria Inta <fitriainta07@gmail.com>
2026-05-08 02:38:43 +0700

    update data

--- models/Menu.php
+++ models/Menu.php
@@ -3,17 +3,17 @@ class Menu {
     public function getMenu() {
         return [
             // BREAD
-            ["nama" => "Garlic Bread",           "harga" => "18K", "gambar" => "garlic bread.jpeg",           "kategori" => "bread"],
-            ["nama" => "Cheese Potato Bread",    "harga" => "22K", "gambar" => "cheese potato bread.jpeg",    "kategori" => "bread"],
-            ["nama" => "Chocolate Banana Bread", "harga" => "24K", "gambar" => "chocolate banana bread.jpeg", "kategori" => "bread"],
+            ["nama" => "Garlic Bread",           "harga" => "18K", "gambar" => "Garlic Bread.jpg",           "kategori" => "bread"],
+            ["nama" => "Cheese Potato Bread",    "harga" => "22K", "gambar" => "Cheese Potato Bread.jpg",    "kategori" => "bread"],
+            ["nama" => "Chocolate Banana Bread", "harga" => "24K", "gambar" => "Chocolate Banana Bread.jpg", "kategori" => "bread"],
             // CAKE
-            ["nama" => "Cheesecake",             "harga" => "35K", "gambar" => "cheesecake.jpeg",             "kategori" => "cake"],
-            ["nama" => "Tiramisu Cake",          "harga" => "38K", "gambar" => "tiramisu cake.jpeg",          "kategori" => "cake"],
-            ["nama" => "Red Velvet Cake",        "harga" => "36K", "gambar" => "red velvet cake.jpeg",        "kategori" => "cake"],
+            ["nama" => "Cheesecake",             "harga" => "35K", "gambar" => "Cheesecake.jpg",             "kategori" => "cake"],
+            ["nama" => "Tiramisu Cake",          "harga" => "38K", "gambar" => "Tiramisu Cake.jpg",          "kategori" => "cake"],
+            ["nama" => "Red Velvet Cake",        "harga" => "36K", "gambar" => "Red Velvet Cake.jpg",        "kategori" => "cake"],
             // PASTRY
-            ["nama" => "Butter Croissant",       "harga" => "20K", "gambar" => "butter croissant.jpeg",       "kategori" => "pastry"],
-            ["nama" => "Pain au Chocolat",       "harga" => "23K", "gambar" => "pain au chocolate.jpeg",      "kategori" => "pastry"],
-            ["nama" => "Red Velvet Cinnamon Roll","harga" => "25K", "gambar" => "red velvet cinnamon roll.jpeg","kategori" => "pastry"],
+            ["nama" => "Butter Croissant",       "harga" => "20K", "gambar" => "Butter Croissant.jpg",       "kategori" => "pastry"],
+            ["nama" => "Pain au Chocolat",       "harga" => "23K", "gambar" => "Pain Au Chocolate.jpg",      "kategori" => "pastry"],
+            ["nama" => "Red Velvet Cinnamon Roll","harga" => "25K", "gambar" => "Red Velvet Cinnamon Roll.jpg","kategori" => "pastry"],
         ];
     }
 }
