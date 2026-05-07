21a6f332e98da3a6b3ef827f0c715c21f21d3c16
Fitria Inta <fitriainta07@gmail.com>
2026-05-08 02:24:17 +0700

    update file

---
+++ models/Menu.php
@@ -0,0 +1,19 @@
+<?php
+class Menu {
+    public function getMenu() {
+        return [
+            // BREAD
+            ["nama" => "Garlic Bread",           "harga" => "18K", "gambar" => "garlic bread.jpeg",           "kategori" => "bread"],
+            ["nama" => "Cheese Potato Bread",    "harga" => "22K", "gambar" => "cheese potato bread.jpeg",    "kategori" => "bread"],
+            ["nama" => "Chocolate Banana Bread", "harga" => "24K", "gambar" => "chocolate banana bread.jpeg", "kategori" => "bread"],
+            // CAKE
+            ["nama" => "Cheesecake",             "harga" => "35K", "gambar" => "cheesecake.jpeg",             "kategori" => "cake"],
+            ["nama" => "Tiramisu Cake",          "harga" => "38K", "gambar" => "tiramisu cake.jpeg",          "kategori" => "cake"],
+            ["nama" => "Red Velvet Cake",        "harga" => "36K", "gambar" => "red velvet cake.jpeg",        "kategori" => "cake"],
+            // PASTRY
+            ["nama" => "Butter Croissant",       "harga" => "20K", "gambar" => "butter croissant.jpeg",       "kategori" => "pastry"],
+            ["nama" => "Pain au Chocolat",       "harga" => "23K", "gambar" => "pain au chocolate.jpeg",      "kategori" => "pastry"],
+            ["nama" => "Red Velvet Cinnamon Roll","harga" => "25K", "gambar" => "red velvet cinnamon roll.jpeg","kategori" => "pastry"],
+        ];
+    }
+}
