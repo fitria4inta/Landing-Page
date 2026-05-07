02ea66af39da83751c0af53f1c0fce0daa172120
Fitria Inta <fitriainta07@gmail.com>
2026-05-08 02:21:38 +0700

    update file

---
+++ controllers/HomeController.php
@@ -0,0 +1,8 @@
+<?php
+class HomeController extends Controller {
+    public function index() {
+        $menuModel = $this->model("Menu");
+        $data["menu"] = $menuModel->getMenu();
+        $this->view("home", $data);
+    }
+}
